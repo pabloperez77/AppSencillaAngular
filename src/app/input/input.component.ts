4baa3c1c0eeb3e43e6c9d0820dc8ccd9a275a355
Pablo <pablopereznovoa@gmail.com>
2018-11-29 13:04:35 +0100

    añadidasInterrogaciones

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -2,18 +2,28 @@ import { Component, Output, EventEmitter } from '@angular/core';
 
 
 @Component({
-  selector: 'app-input',
-  templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  selector: "app-input",
+  templateUrl: "./input.component.html",
+  styleUrls: ["./input.component.css"]
 })
 export class InputComponent {
-    @Output() onNewPanel = new EventEmitter<string>()
-    currentPanel = '';
+    @Output() onNewPanel = new EventEmitter<string>();
+    currentPanel = "";
 
     submit(){
       console.log(this.currentPanel);
       this.onNewPanel.emit(this.currentPanel);
       this.currentPanel = '';
+    }
+    makeInitialInterrogation(){
+        if(this.currentPanel !== ""){
+            this.currentPanel= "¿" + this.currentPanel.replace("¿","").replace("?","");
+        }
+    }
+    makeLastInterrogation(){
+        if (this.currentPanel !== ""){
+            this.currentPanel = this.currentPanel + "?";
+        }
     }
 }
 
